bbd98a142f28fe3331381ac49a908c7cf9b37050
sunga08 <eert2260@naver.com>
2021-01-16 23:39:51 +0900

    solved_210116

--- Programmers/Greedy_체육복.cpp
+++ Programmers/Greedy_체육복.cpp
@@ -17,30 +17,18 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
     int cnt = 0, k = 0, m = 0;
     int answer = n - lost.size();
 
+    for (int i = 0; i < lost.size(); i++) { //lost 배열
 
-    for (int i = 0; i < lost.size(); i++) {
-        for (int j = i + 1; j < lost.size(); j++) {
-            if (lost[i] > lost[j]) {
-                int temp = lost[i];
-                lost[i] = lost[j];
-                lost[j] = temp;
-                break;
-            }
-        }
-    }
+        for (int j = 0; j < reserve.size(); j++) { //reserve 배열
 
-    for (int i = 0; i < lost.size(); i++) {
-
-        for (int j = 0; j < reserve.size(); j++) {
-
-            if (j < reserve.size() - 1 && reserve[j + 1] == lost[i]) {
+            if (j < reserve.size() - 1 && reserve[j + 1] == lost[i]) {  // lost[2,3] , reserve[3,4] 같은 경우 그냥 통과
                 continue;
             }
-            else if (i < lost.size() - 1 && lost[i + 1] == reserve[j]) {
+            else if (i < lost.size() - 1 && lost[i + 1] == reserve[j]) { // lost[3,4] , reserve[2,3] 같은 경우 그냥 통과
                 continue;
             }
 
-            if (abs(lost[i] - reserve[j]) <= 1) {
+            if (abs(lost[i] - reserve[j]) <= 1) { //두 배열 원소의 차가 1이하일 때 빌릴 수 있음(자기 자신을 포함하므로 0 포함)
                 cnt++;
                 reserve[j] = -1;
                 break;
