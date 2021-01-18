e192013b1596e4b4b8b5afedde6db5bd9d8ef461
Sung A <53103434+sunga08@users.noreply.github.com>
2021-01-18 22:33:32 +0900

    Update Greedy_큰 수 만들기.cpp
    안쪽 for문 범위가 관건

--- Programmers/큰 수 만들기/Greedy_큰 수 만들기.cpp
+++ Programmers/큰 수 만들기/Greedy_큰 수 만들기.cpp
@@ -7,40 +7,21 @@ using namespace std;
 string solution(string number, int k) {
     string answer = "";
     int len = number.length();
-    int max = -1, idx, cnt = len - k, n = 0; //cnt=자릿수
-    vector<int> res(len - k);
-
-    for (int i = 0; i <= len - k; i++) {
-        if (max < number[i] - 48) {
-            max = number[i] - 48;
-            idx = i;
-
-        }
-    }
-    // printf("max1: %d \n",max);
-
-    answer.push_back(max + 48);
-
-    cnt--;
-    max = 0;
-    for (int i = 0; i < len - k - 1; i++) { //k개 제거했을 때 숫자 길이-1만큼
-        max = 0;
-        for (int j = idx + 1; j < len; j++) { //max 이후 인덱스 부터 끝까지 돌면서
-            //printf("%d\n",len-cnt);
-            if (j <= (len - cnt) && max < number[j] - 48) {
+    int max = -1, idx=-1;  
+    
+    for (int i = 0; i < len - k ; i++) { //k개 제거했을 때 만들어지는 자릿수 갯수 만큼 돌기
+        max = -1;
+        for (int j = idx + 1; j <= k+i ; j++) { //이전 max 이후 인덱스 부터, 마지막 인덱스에서 남은 자릿수 개수를 뺀 인덱스까지 
+            
+            if (max < number[j] - 48) {
                 max = number[j] - 48;
-                idx = j; //다음 루프에서는 max 이후부터 돌기 위해서 바꿈
-                //printf("idx: %d max: %d \n",idx,max)
+                idx = j; //다음 루프에서는 max 이후부터 돌기 위해서 idx를 바꿈
+                
             }
 
         }
-        cnt--;
-        //printf("max2: %d cnt: %d\n",max,cnt);
-
-        answer.push_back(max + 48);
+        answer+=max+48; //숫자를 다시 문자로 바꾸기 위해 +48
     }
-
-    //cout<<"answer: "+answer;
 
     return answer;
 }
